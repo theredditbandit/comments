19522f1752e07097d609b11d3830ff89fc19facb
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2023-01-07 20:30:20 +0530

    Add getid function and related tests

--- main.py
+++ main.py
@@ -6,15 +6,31 @@ def theinput():
     return input(c.LIGHTGREEN_EX + "Enter a Youtube URL >> ")
 
 
-def isvalid(URL):
-    if re.search(r"youtube.com/watch\?v",URL):
+def isvalid(url):
+    """_summary_ : Validate whether the given url belongs to a youtube video or not
+
+    Args:
+        url (_str_): The video url
+
+    Returns:
+        _bool_
+    """
+    if re.search(r"youtube.com/watch\?v",url) or re.search(r"youtu.be",url):
         return True
     else:
         print(c.RED + "Please enter the URL to a Youtube Video ",c.WHITE + "Example : https://www.youtube.com/watch?v=dQw4w9WgXcQ",sep="\n")
         return False
 
-def getid(): # to get video ID from the youtube URL for the youtube API
-    ...
+def getid(url):
+    """_summary_ : Extract video ID from the youtube URL for the youtube API
+
+    Args:
+        url (_str_): The video url
+    Returns: The video id
+    """
+    id = re.search(r"^https:\/\/(?:youtu\.be\/|www\.youtube\.com\/watch\?v=)([^&#]+)", url).group(1)
+    print(c.YELLOW + "Video ID Found : " + c.WHITE + id)
+    return id
 
 
 def main():
--- test_pytest.py
+++ test_pytest.py
@@ -8,6 +8,16 @@ def test_isvalid():
     assert isvalid("youtube.com") == False
     assert isvalid("https://www.youtube.com/watch?v=6XYi5nSu_P0&list=WL&index=5&t=344s") == True
     assert isvalid("https://www.youtube.com/watch?v=dQw4w9WgXcQ") == True
+    assert isvalid("https://youtu.be/fT2KhJ8W-Kg") == True
 
-def test_getid():
-    ...
+def test_getid__format_youtube():
+    assert getid("https://www.youtube.com/watch?v=dQw4w9WgXcQ") == "dQw4w9WgXcQ"
+    assert getid("https://www.youtube.com/watch?v=LeSRHmKjNnM") == "LeSRHmKjNnM"
+
+def test_getid__format_youtube_WL_T():
+    assert getid("https://www.youtube.com/watch?v=6XYi5nSu_P0&list=WL&index=5&t=344s") == "6XYi5nSu_P0"
+    assert getid("https://www.youtube.com/watch?v=gKgSvAyPFlU&list=WL&index=6") == "gKgSvAyPFlU"
+
+def test_youtu_do_be():
+    assert getid("https://youtu.be/fT2KhJ8W-Kg") == "fT2KhJ8W-Kg"
+    assert getid("https://youtu.be/LeSRHmKjNnM") == "LeSRHmKjNnM"
